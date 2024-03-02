bee9ea28a0f69d3477be437e6d0970e7b58fac96
Ahmet Karayel <ahmetp.karayel06@gmail.com>
2024-03-02 11:35:00 +0300

    project created

---
+++ lib/presentation/screens/edit_image/screen_edit_image.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScreenEditImage extends StatefulWidget {
+  const ScreenEditImage({super.key});
+
+  @override
+  State<ScreenEditImage> createState() => _ScreenEditImageState();
+}
+
+class _ScreenEditImageState extends State<ScreenEditImage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
